31e4d4d3c578e107c85eb69760cacc32ac3407e1
Neville Dastur <neville@dastur.me.uk>
2013-10-22 16:37:11 +0100

    add ios7 icons

--- makeTiIcons.bash
+++ makeTiIcons.bash
@@ -20,13 +20,25 @@ fi
 
 # iOS app icons
 echo "Making the iOS icon images"
-convert "$1" -resize 512x512 $PREFIX/iphone/appicon@512.png
-convert "$1" -resize 144x144 $PREFIX/iphone/appicon-144.png
+# Input should be the 512 icon
+convert "$1" -resize 512x512 $PREFIX/iphone/iTunesArtwork
+#convert "$1" -resize 512x512 $PREFIX/iphone/appicon@512.png
 convert "$1" -resize 114x114 $PREFIX/iphone/appicon@2x.png
 convert "$1" -resize 72x72   $PREFIX/iphone/appicon-72.png
+convert "$1" -resize 144x144   $PREFIX/iphone/appicon-72@2x.png
 convert "$1" -resize 57x57   $PREFIX/iphone/appicon.png
 convert "$1" -resize 50x50   $PREFIX/iphone/appicon-Small-50.png
+convert "$1" -resize 100x100   $PREFIX/iphone/appicon-Small-50@2x.png
 convert "$1" -resize 29x29   $PREFIX/iphone/appicon-Small.png
+convert "$1" -resize 58x58   $PREFIX/iphone/appicon-Small@2x.png
+
+# new iOS 7 icons
+echo "Making the iOS 7 icon images"
+convert "$1" -resize 40x40 $PREFIX/iphone/appicon-Small-40.png
+convert "$1" -resize 80x80 $PREFIX/iphone/appicon-Small-40@2x.png
+convert "$1" -resize 120x120 $PREFIX/iphone/appicon-60@2x.png
+convert "$1" -resize 76x76 $PREFIX/iphone/appicon-76.png
+convert "$1" -resize 152x152 $PREFIX/iphone/appicon-76@2x.png
 
 # Android
 echo "Making the Android icon images"
